6cd46b84fd4fece9e7556249043cc97b3d72c27c
Miguel S. da Silva <miguels.dasilva00@gmail.com>
2021-12-03 09:47:02 -0300

    Implement DrawerLayout actions
    - Configure the drawerLayout to change fragments according to the selected action, as well as changing the Action Bar name to the corresponding fragment.
    
    Signed-off-by: Miguel S. da Silva <miguels.dasilva00@gmail.com>

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/MainActivity.kt
@@ -2,10 +2,58 @@ package co.tiagoaguiar.tutorial.jokerappdev
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-  }
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfig: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setActionBarConfiguration()
+    }
+
+    private fun setActionBarConfiguration() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        createDrawerLayoutConfig()
+    }
+
+    private fun createDrawerLayoutConfig() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.main_nav_view)
+
+        initiateController()
+        passNameAndOpenFunctionalityTo(drawerLayout)
+        addControllerTo(navView)
+    }
+
+    private fun initiateController() {
+        navController = findNavController(R.id.nav_host_fragment_content_main)
+    }
+
+    private fun passNameAndOpenFunctionalityTo(drawerLayout: DrawerLayout) {
+        appBarConfig = AppBarConfiguration(
+            setOf(R.id.nav_home, R.id.nav_joke_day, R.id.nav_about),
+            drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    private fun addControllerTo(navView: NavigationView) =
+        navView.setupWithNavController(navController)
+
+    override fun onSupportNavigateUp(): Boolean =
+        navController.navigateUp(appBarConfig) || super.onNavigateUp()
+
 }
