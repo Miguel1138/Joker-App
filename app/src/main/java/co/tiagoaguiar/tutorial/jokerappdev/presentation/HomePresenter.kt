6ff79e827e7bd1d4153e06f5e67e71ee39eaabed
Miguel S. da Silva <miguels.dasilva00@gmail.com>
2022-01-04 11:23:18 -0300

    Create Gradient algorithm
    - Categories now have different gradient for each category.
    
    Signed-off-by: Miguel S. da Silva <miguels.dasilva00@gmail.com>

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/presentation/HomePresenter.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/presentation/HomePresenter.kt
@@ -1,5 +1,6 @@
 package co.tiagoaguiar.tutorial.jokerappdev.presentation
 
+import android.graphics.Color
 import co.tiagoaguiar.tutorial.jokerappdev.datasource.CategoryRemoteDatasource
 import co.tiagoaguiar.tutorial.jokerappdev.datasource.ListCategoryCallback
 import co.tiagoaguiar.tutorial.jokerappdev.model.JokeCategory
@@ -16,10 +17,24 @@ class HomePresenter(
     }
 
     override fun onSuccess(response: List<String>) {
-        val categories = response.map { category ->
-            JokeCategory(category, 0xffff0000)
-        }
+        val categories = inflateJokeCategoriesBy(response)
         view.showCategories(categories)
+    }
+
+    private fun inflateJokeCategoriesBy(response: List<String>): List<JokeCategory> {
+        val gradientStart = 40
+        val gradientEnd = 190
+        val gradientDiff = (gradientEnd - gradientStart) / response.size
+
+        return response.mapIndexed { index, category ->
+            val hsv = floatArrayOf(
+                gradientStart + (gradientDiff * index).toFloat(),
+                100.0f,
+                100.0f
+            )
+            val finalGradient = Color.HSVToColor(hsv).toLong()
+            JokeCategory(category, finalGradient)
+        }
     }
 
     override fun onError(message: String) = view.showFailure(message)
